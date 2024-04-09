5225f9c29ea4947304cb63c013979c971b88eec4
DenersonHenrique <denersonh@gmail.com>
2024-04-09 12:14:52 -0300

    [main] Create Flutter Project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# trip_manager_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/br/com/denin/u10/trip_manager_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.denin.u10.trip_manager_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
